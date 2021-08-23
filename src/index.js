ff563f24c5af895c3d7b158cc40b2c775f86b3b0
DenisKhmelnov <denishmelnov@gmail.com>
2021-08-23 23:51:16 +0500

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+
+    if(arguments.length === 0 || array.length === 0) return 0;
+
+    let min = array[0];
+    for(let i of array){
+        if (i < min) min = i;
+    }
+
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(arguments.length === 0 || array.length === 0) return 0;
+
+    let max = array[0];
+    for(let i of array){
+        if (i > max) max = i;
+    }
+
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(arguments.length === 0 || array.length === 0) return 0;
+
+    let avg = 0;
+    for(let i of array) {
+        avg += i;
+    }
+    return avg / array.length;
 }
